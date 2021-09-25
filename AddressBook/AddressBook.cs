28c7293b30a5bc74ecfd94153aa85598c951dc99
nibedita260 <deepi.nibedita260.com>
2021-09-25 10:57:03 +0530

    [Nibedita]:Added Create Contacts AddressBook Features Implemented

---
+++ AddressBook/AddressBook.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class AddressBook
+    {
+        public void CreateContacts()
+        {
+            Contacts contacts = new Contacts();
+            Console.WriteLine("Enter Firstname");
+            contacts.Firstname = Console.ReadLine();
+            Console.WriteLine("Enter Lastname");
+            contacts.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address");
+            contacts.Address = Console.ReadLine();
+            Console.WriteLine("Enter City");
+            contacts.City = Console.ReadLine();
+            Console.WriteLine("Enter State");
+            contacts.State = Console.ReadLine();
+            Console.WriteLine("Enter Zipcode");
+            contacts.Zip = Console.ReadLine();
+            Console.WriteLine("Enter Phonenumber");
+            contacts.PhoneNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Email");
+            contacts.Email = Console.ReadLine();
+        }
+        
+    }
+}
---
+++ AddressBook/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook addressBook = new AddressBook();
+            addressBook.CreateContacts();
+        }
+    }
+}

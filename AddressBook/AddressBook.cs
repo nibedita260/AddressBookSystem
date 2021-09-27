58f0a879671df3079006e3d6f2005cbbd8f163e9
nibedita260 <deepi.nibedita260.com>
2021-09-27 18:53:11 +0530

    [Nibedita]:Added UniqueContacts AddressBook Feature Implemented

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -30,30 +30,154 @@ namespace AddressBook
             contacts.Email = Console.ReadLine().ToUpper();
             GetContacts.Add(contacts);
             Console.WriteLine("contacts details added successfully");
-            //creating multiple contacts
-            Contacts contacts2 = new Contacts();
+        }
+        public void Print(Contacts contacts)
+        {
             Console.WriteLine("Enter Firstname");
-            contacts2.Firstname = Console.ReadLine().ToUpper();
+            contacts.Firstname = Console.ReadLine().ToUpper();
             Console.WriteLine("Enter Lastname");
-            contacts2.LastName = Console.ReadLine().ToUpper();
+            contacts.LastName = Console.ReadLine().ToUpper();
             Console.WriteLine("Enter Address");
-            contacts2.Address = Console.ReadLine().ToUpper();
+            contacts.Address = Console.ReadLine().ToUpper();
             Console.WriteLine("Enter City");
-            contacts2.City = Console.ReadLine().ToUpper();
+            contacts.City = Console.ReadLine().ToUpper();
             Console.WriteLine("Enter State");
-            contacts2.State = Console.ReadLine().ToUpper();
+            contacts.State = Console.ReadLine().ToUpper();
             Console.WriteLine("Enter Zipcode");
-            contacts2.Zip = Convert.ToInt32(Console.ReadLine());
+            contacts.Zip = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter Phonenumber");
-            contacts2.PhoneNumber = Convert.ToInt32(Console.ReadLine());
+            contacts.PhoneNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter Email");
-            contacts2.Email = Console.ReadLine().ToUpper();
-            GetContacts.Add(contacts2);
-            Console.WriteLine("contacts2 details added successfully");
-
+            contacts.Email = Console.ReadLine().ToUpper();
+        }
+        public void DisplayContacts()
+        {
+            foreach (var data in GetContacts)
+            {
+                if (GetContacts.Contains(data))
+                {
+                    Print(data);
+                    Console.WriteLine("The Contact Details are\n:" + data.Firstname + " " + data.LastName + " " + data.Address + " " + data.City + " " + data.State + " " + data.Zip + " " + data.PhoneNumber + " " + data.Email);
+                }
+                else
+                {
+                    Console.WriteLine("contact does not exists");
+                }
+            }
+        }
+        public void EditContacts()
+        {
+            CreateContacts();
+            Console.WriteLine("to edit contact list enter contact firstname");
+            string name = Console.ReadLine().ToLower();
+            foreach (var data in GetContacts)
+            {
+                if (GetContacts.Contains(data))
+                {
+                    if (data.Firstname == name)
+                    {
+                        Console.WriteLine("name exists");
+                        Console.WriteLine("To edit contacts enter 1.LastName 2.Address 3.City 4.State 5.Zip 6.PhoneNumber 7.Email");
+                        int options = Convert.ToInt32(Console.ReadLine());
+                        switch (options)
+                        {
+                            case 1:
+                                string lastname = Console.ReadLine();
+                                break;
+                            case 2:
+                                string address = Console.ReadLine();
+                                data.Address = address;
+                                break;
+                            case 3:
+                                string city = Console.ReadLine();
+                                data.City = city;
+                                break;
+                            case 4:
+                                string state = Console.ReadLine();
+                                data.State = state;
+                                break;
+                            case 5:
+                                int zip = Convert.ToInt32(Console.ReadLine());
+                                data.Zip = zip;
+                                break;
+                            case 6:
+                                int phno = Convert.ToInt32(Console.ReadLine());
+                                data.PhoneNumber = phno;
+                                break;
+                            case 7:
+                                string email = Console.ReadLine();
+                                data.Email = email;
+                                break;
+                            default:
+                                Console.WriteLine("choose valid option");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("name does not exists");
+                    }
+                }
+            }
+            DisplayContacts();
+        }
+        public void DeleteContacts()
+        {
+            CreateContacts();
+            Console.WriteLine("to delete contact list enter contact firstname ");
+            string name = Console.ReadLine().ToLower();
+            foreach (var data in GetContacts)
+            {
+                if (GetContacts.Contains(data))
+                {
+                    if (data.Firstname == name)
+                    {
+                        try
+                        {
+                            Console.WriteLine("given name contact exists");
+                            GetContacts.Remove(data);
+                            Console.WriteLine("contact deleted successfully");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("given name contact does not exists");
+                    }
+                }
+            }
+            DisplayContacts();
+        }
+        public void AddMultipleContacts(int n)
+        {
+            while (n > 0)
+            {
+                Console.WriteLine("enter name to store the above contact details in a dictionary");
+                string name = Console.ReadLine().ToLower();
+                CreateContacts();
+                dict.Add(name, GetContacts);
+                n--;
+            }
+        }
+        public void DisplayMultContacts()
+        {
             //maintain dictionary of addressbook name to addressbook
-            dict.Add("Nibedita", GetContacts);
-            dict.Add("Abhinav", GetContacts);
+            Console.WriteLine("enter name of dictionary to display that contact details");
+            string name = Console.ReadLine().ToLower();
+            foreach (var contacts in dict)
+            {
+                if (dict.ContainsKey(name))
+                {
+                    Console.WriteLine("contact details of " +contacts);
+                }
+                else
+                {
+                    Console.WriteLine("contact details of " + dict.Keys + "are found");
+                }
+            }
         }
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AddressBook
 {
@@ -7,10 +8,36 @@ namespace AddressBook
         static void Main(string[] args)
         {
             bool isExit = false;
+            int options;
             while (!isExit)
             {
-                AddressBook addressBook = new AddressBook();
-                addressBook.CreateContacts();
+                Console.WriteLine("Choose 1.CreateContacts 2.DisplayContacts 3.EditContacts 4.DeleteContacts 5.AddMultipleContacts 6.UniqueContacts");
+                options = Convert.ToInt32(Console.ReadLine());
+                AddressBook addressBook1 = new AddressBook();
+                switch (options)
+                {
+                    case 1:
+                        addressBook1.CreateContacts();
+                        break;
+                    case 2:
+                        addressBook1.DisplayContacts();
+                        break;
+                    case 3:
+                        addressBook1.EditContacts();
+                        break;
+                    case 4:
+                        addressBook1.DeleteContacts();
+                        break;
+                    case 5:
+                        addressBook1.AddMultipleContacts(2);
+                        break;
+                    case 6:
+                        addressBook1.DisplayMultContacts();
+                        break;
+                    default:
+                        Console.WriteLine("Choose valid option");
+                        break;
+                }
             }
         }
     }

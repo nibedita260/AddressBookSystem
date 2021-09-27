dd8216b26a84992b623448631d2d1e5324fe294e
nibedita260 <deepi.nibedita260.com>
2021-09-27 12:16:20 +0530

    [Nibedita]:Added DeleteContacts AddressBook Feature Implemented

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -7,12 +7,26 @@ namespace AddressBook
         static void Main(string[] args)
         {
             bool isExit = false;
+            int options;
             while (!isExit)
             {
-                AddressBook addressBook = new AddressBook();
-                addressBook.CreateContacts();
-                //addressBook.EditContacts("NIBI");
-                addressBook.DeleteContacts("NIBI");
+                Console.WriteLine("Choose 1.CreateContacts 2.DisplayContacts 3.EditContacts 4.DeleteContacts");
+                options = Convert.ToInt32(Console.ReadLine());
+                AddressBook addressBook1 = new AddressBook();
+                switch (options)
+                {
+                    case 1:addressBook1.CreateContacts();
+                        break;
+                    case 2:addressBook1.DisplayContacts();
+                        break;
+                    case 3:addressBook1.EditContacts();
+                        break;
+                    case 4:addressBook1.DeleteContacts();
+                        break;
+                    default:
+                        Console.WriteLine("Choose valid option");
+                        break;
+                }
             }
         }
     }

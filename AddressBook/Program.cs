5cc324c4761e64e6cf80ab56944fc8391b31be27
nibedita260 <deepi.nibedita260.com>
2021-10-19 15:19:09 +0530

    [Nibedita] Add: ReadOrWriteContactsIntoTxtFile For AddressBookSystem Features Implemented

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -12,7 +12,7 @@ namespace AddressBook
             AddressBook addressBook1 = new AddressBook();
             while (!isExit)
             {
-                Console.WriteLine("Choose 1.CreateContacts 2.DisplayContacts 3.EditContacts 4.DeleteContacts 5.AddMultipleContacts 6.AddUniqueContacts 7.DisplayUniqueContacts 8.SearchMultiplePersonNamesInCityOrState 9.DisplayContactsByCities 10.CountContactsByCities 11.SortContactsByName 12.SortContactsByCityStateZip");
+                Console.WriteLine("Choose 1.CreateContacts 2.DisplayContacts 3.EditContacts 4.DeleteContacts 5.AddMultipleContacts 6.AddUniqueContacts 7.DisplayUniqueContacts 8.SearchMultiplePersonNamesInCityOrState 9.DisplayContactsByCities 10.CountContactsByCities 11.SortContactsByName 12.SortContactsByCityStateZip 13.ReadOrWriteContactsToTxtFile");
                 options = Convert.ToInt32(Console.ReadLine());
                 switch (options)
                 {
